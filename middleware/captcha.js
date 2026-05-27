81d4921a71d3a0da991482132d4dea85de583cee
ejmrp <ofc.baddu@gmail.com>
2026-05-28 01:26:21 +0700

    Create captcha.js

---
+++ middleware/captcha.js
@@ -0,0 +1,23 @@
+export async function verifyCaptcha(token) {
+
+  const body = new URLSearchParams();
+
+  body.append(
+    'secret',
+    process.env.TURNSTILE_SECRET
+  );
+
+  body.append('response', token);
+
+  const res = await fetch(
+    'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+    {
+      method: 'POST',
+      body
+    }
+  );
+
+  const data = await res.json();
+
+  return data.success === true;
+}
