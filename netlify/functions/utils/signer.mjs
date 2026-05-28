4dbba5fddbf2d270f6e16ea056b2bb79d7ccdced
ejmrp <ofc.baddu@gmail.com>
2026-05-28 08:56:35 +0700

    Update signer.mjs

--- netlify/functions/utils/signer.mjs
+++ netlify/functions/utils/signer.mjs
@@ -1,26 +1,38 @@
-import nacl from 'tweetnacl';
+import * as nacl
+from 'tweetnacl';
 
 function b64(bytes){
+
   return Buffer
     .from(bytes)
     .toString('base64');
+
 }
 
 export async function signTx(message){
 
   const seed = Buffer.from(
-    process.env.FAUCET_PRIVATE_KEY,
+
+    process.env
+    .FAUCET_PRIVATE_KEY,
+
     'base64'
+
   );
 
   const pair =
-    nacl.sign.keyPair.fromSeed(seed);
+    nacl.sign.keyPair
+    .fromSeed(seed);
 
   const sig =
     nacl.sign.detached(
+
       Buffer.from(message),
+
       pair.secretKey
+
     );
 
   return b64(sig);
+
 }
